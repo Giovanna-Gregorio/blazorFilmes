8f543e63a80a99a24d9dbb1cf77b1025eaa0507e
Giovanna-Gregorio <giovanna_gregorio@univem.edu.br>
2020-09-16 20:12:17 -0300

    Alteração no cadastro de usuário, controller, pacotes do entity framework e connection string

---
+++ Server/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+using BlazorFilmes.Shared;
+using Microsoft.AspNetCore.Mvc; 
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : Controller
+    {
+        [HttpGet]
+        [Route("List")]
+        public IEnumerable<Usuario> Get()
+        {
+            List<Usuario> usuarios = new List<Usuario>()
+            {
+                new Usuario() { Title = "Sr", FirstName = "Joao", LastName = "das Cove",
+                                Email = "teste@teste.com", DateBirth = DateTime.Now, Password = "123456",
+                                ConfirmPassword = "123456", Accept = true },
+
+                new Usuario() { Title = "Sr", FirstName = "Joao2", LastName = "das Cove2",
+                                Email = "teste@teste.com", DateBirth = DateTime.Now, Password = "123456",
+                                ConfirmPassword = "123456", Accept = true },
+            };
+
+            return usuarios;
+        }
+
+        //Action/Metodo para adicionar/Post um Usuário
+        [HttpPost]
+        [Route("Create")]
+        public async Task<ActionResult> Post([FromBody]Usuario usuario)
+        {
+            //TODO: Adicionar novo usuario (user) uma lista
+            Console.WriteLine("teste", usuario.ToString());
+            return null;    
+        }
+    }
+}
